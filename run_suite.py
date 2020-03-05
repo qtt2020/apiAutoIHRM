9d10ec2e5ee645bc60c171d93be58e96c7a16279
qtt <qtt@email.com>
2020-03-05 11:33:34 +0800

    修改报告路径

--- run_suite.py
+++ run_suite.py
@@ -15,7 +15,8 @@ suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestEmployeeParams))
 suite.addTest(unittest.makeSuite(TestLogin))
 # 设置路径
-filename = BASE_DIR+"/report/emp_{}.html".format(time.strftime("%Y%m%d%H%M%S"))
+# filename = BASE_DIR+"/report/emp_{}.html".format(time.strftime("%Y%m%d%H%M%S"))
+filename = BASE_DIR+"/report/ihrm_emp.html"
 # 打开测试报告文件
 with open(filename, "wb") as f:
     # 实例化HTMLTestRunner对象
